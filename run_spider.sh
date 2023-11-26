0ab413f4c535b34b88b2fbfa86d54e5c24237e55
AndreyM <222mushkin@gmail.com>
2023-11-26 18:49:09 +0300

    Fixed bug that php script were executed by 'root'

--- run_spider.sh
+++ run_spider.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 readonly PROJECT_DIR=$(dirname "$(readlink -f "$0")")
 readonly MOUNTED_PROJECT_DIR="/app"
+readonly USER_ID=$(id -u)
 
 if [[ -z $1 || -z $2 ]]; then
   echo "Specify URL and regular expression to search."
@@ -18,5 +19,6 @@ docker run \
   --rm \
   --volume $PROJECT_DIR:$MOUNTED_PROJECT_DIR \
   --workdir $MOUNTED_PROJECT_DIR \
+  --user $USER_ID:$USER_ID \
   php:8.1-cli \
   php -f getcsv.php $URL $REGEX
